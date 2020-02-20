fc46e5f151c274ad040b654611fd3e63e2a3eff1
Ryan C. Scott <ryan.c.scott@nasa.gov>
2020-02-20 12:03:21 -0500

    Combined AODs into 7 types + total

--- read_hourly_match.f90
+++ read_hourly_match.f90
@@ -1,6 +1,6 @@
 !=================================================================== 
 ! Program reads aerosol profiles from MATCH-hourly netCDF files
-!! 
+! 
 ! Name: read_hourly_match.f90
 !
 ! Modules used: netcdf
@@ -19,7 +19,7 @@
 ! 
 ! (2) Combine constituents & extract what's needed for Fu-Liou 
 !
-! (3) Match the aerosol data to specific CERES FOVs ?
+! (3) DONE: Match the aerosol data to specific CERES FOVs ?
 !     This include HOUR time step and LAT/LON
 !
 !===================================================================
@@ -45,30 +45,36 @@ integer :: latvarid, lonvarid    ! lat/lon variable ID
 integer :: ncid                  ! nc file ID
 integer :: i, j, p, k, t         ! loop indices
 
-! CERES fov lat, lon
-real :: fov_lon = 145
+! Artificial data
+! Extract MATCH data at the time and location of each SSF FOV
+! CERES fov lat, lon, hr - these will need to be read in
+real :: fov_lon = -145
 real :: fov_lat = -20
+integer :: fov_hr = 0
 
 !=====================================================
 
-! MATCH aerosol vertical profile (AVP) data structure
-type avptype
+! MATCH aerosol vertical profile (mavp) data structure
+type mavptype
 real :: aero_prof(11,nlon,nlat,nlev,ntime)  ! aerosol profile array, types 1-11
+real :: aero_prof7(7,nlon,nlat,nlev,ntime)  ! aerosol profile array, types 1-11 combined into 7 categories
 character*7 aero_type11(11)                 ! aerosol profile type label, 11 constituents
 character*7 aero_type7(7)                   ! aerosol profile type label, 7 constituents obtained by combining categories
-end type avptype
+end type mavptype
 
-type (avptype) mpro                         ! MATCH profile data structure
+type (mavptype) mpro                        ! MATCH profile data structure
  
 ! MATCH aerosol optical depth (AOD) data structure
 type aodtype
 real :: aod_array(12,nlon,nlat,ntime)       ! AOD array, total + 11 aerosol types
+real :: aod_array8(8,nlon,nlat,ntime)       ! AOD array, total + 7 combined aerosol types
 character*8 aod_type12(12)                  ! AOD type label, total + 11 aerosol types
+character*8 aod_type8(8)                    ! AOD type label, total + 7 combined aerosol types
 end type aodtype
 
 type (aodtype) aod                          ! AOD data structure
 
-integer :: iprofvarid(1:11)                  ! integer aerosol profile variable id, 11 aerosol types
+integer :: iprofvarid(1:11)                 ! integer aerosol profile variable id, 11 aerosol types
 integer :: iaodvarid(1:12)                  ! integer aod variable id, total + 11 aerosol types
        
 !=====================================================
@@ -87,7 +93,8 @@ data mpro%aero_type11 / &
 'OCPHO',         & !10
 'VOLC' /           !11
 
-data mpro%aero_type7 / &
+data aod%aod_type8 / &
+'Total',   &    
 'DustSm',  &
 'DustLg',  &
 'SO4',     &
@@ -194,18 +201,42 @@ do t = 1,11            ! aerosol type
 end do
 
 
+print*, "======================================="
+print*, "Combining AODs into 7 types + total AOD"
+print*, "======================================="
+
+aod%aod_array8(1,:,:,:) = SUM(aod%aod_array(2:12,:,:,:),1) !Total - should equal AEROOD
+aod%aod_array8(2,:,:,:) = aod%aod_array(2,:,:,:)           !DustSm
+aod%aod_array8(3,:,:,:) = SUM(aod%aod_array(3:5,:,:,:),1)  !DustLg
+aod%aod_array8(4,:,:,:) = aod%aod_array(6,:,:,:)           !SO4
+aod%aod_array8(5,:,:,:) = aod%aod_array(7,:,:,:)           !SSLT - Sea Salt
+aod%aod_array8(6,:,:,:) = SUM(aod%aod_array(8:9,:,:,:),1)  !SOOT - Black Carbon
+aod%aod_array8(7,:,:,:) = aod%aod_array(10,:,:,:)          !Solub
+aod%aod_array8(8,:,:,:) = aod%aod_array(11,:,:,:)          !Insolub
+
+!print*, "Computed total AOD"
+!print*, aod%aod_array8(1,1:10,1,1)
+!print*, "Read from file"
+!print*, aod%aod_array(1,1:10,1,1)
+
 print*, "======================================"
 
-print*, "Latitude centers"
+print*, "MATCH Latitude centers"
 print*, lat
 
-print*, "Longitude centers"
+print*, "MATCH Longitude centers"
 print*, lon
 
-print*, "Pressure levels"
+print*, "MATCH Pressure levels"
 print*, plev
 
 print*, "======================================"
+print*, "Matching MATCH to CERES footprints... "
+print*, "======================================"
+
+
+! CERES FOV hr is offset from MATCH by 1
+fov_hr = fov_hr + 1
 
 print*, "Artificial CERES FOV lon:", fov_lon
 print*, "Artificial CERES FOV lon:", fov_lat
@@ -222,8 +253,8 @@ print*, "======================================"
 
 print*, "Getting aerosol profile at CERES FOV location..."
 
-do t = 1,11      ! aerosol type                                                                                              
-  do k = 1,1     ! time                                                                                                        
+do t = 1,11              ! aerosol type                                                                                              
+  do k = fov_hr,fov_hr   ! time
     print*, "============================================================================"
     print*, "Hour:",k, "Type:  ",mpro%aero_type11(t), "VarID:  ",iaodvarid(t)
     print*, "============================================================================"
@@ -234,14 +265,17 @@ do t = 1,11      ! aerosol type
   end do
 end do
 
-
 ! close the file, freeing all resources
 call check( nf90_close(ncid) )
 
-! ================================================================
+!================================================================
 
 contains
 
+!**********************************************************                                                                     
+! SUBROUTINE:                                                                                                                          
+! Check netcdf-fortran functions for errors
+!**********************************************************
 subroutine check(status)
 integer, intent (in) :: status
 
@@ -251,56 +285,42 @@ if(status /= nf90_noerr) then
     end if
 end subroutine check
 
-
-! Random function to make sure it's working...
-function match_lon(ceres_lon) result(z)                                                                                                
-!implicit none                                                                                                                        
-real :: z                                                                                                                         
-real :: ceres_lon
-z = ceres_lon + 1.0                                                                                                               
-end function match_lon
-
 !**********************************************************
 ! FUNCTION:
-! Calculate distance between CERES FOV lon and MATCH lons
-! and match them by index
+! Calculate CERES FOV & MATCH long distance, match by index
 !********************************************************** 
 function lon_match_ceresfov(fovlon) result(ilon)
 integer :: i            !loop index
 integer :: ilon         !min dist index
 real :: dist_lon(nlon)  !distance array
 real :: fovlon          !fov lon
-! compute distance between CERES FOV lon and MATCH lon centers
+
+! if CERES FOV lon < 0 convert range to 0-360
+if (fovlon<0) fovlon = fovlon+360
+! compute distance between CERES FOV & MATCH lon centers
 dist_lon = abs(lon-fovlon)
 ! find the minimum distance and extract index
 do i=1,nlon
-if (dist_lon(i) == minval(dist_lon)) ilon = i 
+  if (dist_lon(i) == minval(dist_lon)) ilon = i 
 end do
 end function lon_match_ceresfov
 
 !********************************************************** 
 ! FUNCTION                                                                        
-! Calculate distance between CERES FOV lon and MATCH lons                                                                             
-! and match them by index                                   
+! Calculate CERES FOV & MATCH lat distance, match by index
 !**********************************************************                                                                           
 function lat_match_ceresfov(fovlat) result(ilat)
 integer :: i            !loop index      
 integer :: ilat         !index of profile closest to fov                                                                          
 real :: dist_lat(nlat)  !distance array                                                                                             
-real :: fovlat          !fov lon                                                                                                      
-! compute distance between CERES FOV lon and MATCH lon centers                                                                        
+real :: fovlat          !fov lat                                                                                                      
+! compute distance between CERES FOV & MATCH lat centers                                                                        
 dist_lat = abs(lat-fovlat)
 ! find the minimum distance and extract index                                                                                         
 do i=1,nlat
-if (dist_lat(i) == minval(dist_lat)) ilat = i
+  if (dist_lat(i) == minval(dist_lat)) ilat = i
 end do
 end function lat_match_ceresfov
-
-
-
-
-
-
 
 
 !********************************************************** 
